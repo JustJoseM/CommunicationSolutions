2bf88094a23a98536477a618695e30811d37eb37
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-04-26 21:11:45 -0700

    added margins to .Description in Testimonial.css to avoid empty ruleset

--- src/Pages/Testimonial.js
+++ src/Pages/Testimonial.js
@@ -43,9 +43,7 @@ function Testimonial() {
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </p>
                         </div>
-                        
                     </div>
-                    
                     </div>
                 </div>
             </div>
