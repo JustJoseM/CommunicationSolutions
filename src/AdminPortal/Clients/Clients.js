4e0ae7688cfdd2b52fc2214e50caa450126c3572
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-09-06 22:20:45 -0700

    Created Client css and js and renamed Admin homepage js and css files to be more descriptive.

---
+++ src/AdminPortal/Clients/Clients.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Clients.css';
+
+function Users() {
+    return (
+        <div className="ClientPage">
+            <h1>Client information here</h1>
+        </div>
+    )
+}
---
+++ src/AdminPortal/Home/AdminHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './AdminHome.css';
+
+function AdminHome() {
+    return(
+        <div className="AdminHome">
+            <h1>Welcome to Admin Portal</h1>
+        </div>
+    )
+}
+
+export default AdminHome;
