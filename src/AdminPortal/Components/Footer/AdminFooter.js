529224a494fa7d45c144347fcfdf20c61e7f3355
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-09-06 23:42:29 -0700

    Created Admin Portal Compents; Menu, Navbar, and Footer placeholders.

---
+++ src/AdminPortal/Components/Footer/AdminFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AdminFooter.css";
+
+function AdminFooter() {
+    return(
+        <div className="footer">Footer</div>
+    )
+}
+
+export default AdminFooter;
