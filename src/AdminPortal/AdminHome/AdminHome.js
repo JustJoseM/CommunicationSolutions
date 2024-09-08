af52d3c7f99201cca2188b5e01181a97ffb063ed
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-09-08 00:30:43 -0700

    Placeholder text for boxes on AdminHome

--- src/AdminPortal/AdminHome/AdminHome.js
+++ src/AdminPortal/AdminHome/AdminHome.js
@@ -8,14 +8,12 @@ const AdminHome = () => {
             <div className="box box1">
                 <TopBox />
             </div>
-            <div className="box box2">Box2</div>
-            <div className="box box3">Box3</div>
-            <div className="box box4">Box4</div>
-            <div className="box box5">Box5</div>
-            <div className="box box6">Box6</div>
-            <div className="box box7">Box7</div>
-            <div className="box box8">Box8</div>
-            <div className="box box9">Box9</div>
+            <div className="box box2">Total Number of Consultations</div>
+            <div className="box box3">Total Revenue</div>
+            <div className="box box4">Upcoming Appointments</div>
+            <div className="box box5">Client Satisfaction</div>
+            <div className="box box6">Upcoming Tasks</div>
+            <div className="box box7">Task List/Notes</div>
         </div>
     )
 }
