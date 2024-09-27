9ea575787024b2adf2bc357c21b48b2e546a74b4
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-09-27 15:26:43 -0700

    Created a basic widget. installed chart.js and react-chartjs-2. please install the dependencies from src with the following command: npm install chart.js react-chartjs-2

--- src/AdminPortal/AdminHome/AdminHome.js
+++ src/AdminPortal/AdminHome/AdminHome.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './AdminHome.css';
 import TopBox from "../AdminComponents/TopBox/TopBox";
+import StatisticsWidget from "../AdminWidgets/StatisticsWidget";
 
 const AdminHome = () => {
+
+    const totalConsultations = 15;
+    const totalRevenue = "$1000";
+
     return(
         <div className="AdminHome">
             <div className="box box1">
                 <TopBox />
             </div>
-            <div className="box box2">Total Number of Consultations</div>
-            <div className="box box3">Total Revenue</div>
+            <div className="box box2">
+                <StatisticsWidget title="Total Number of Consultations" value={totalConsultations} />
+            </div>
+            <div className="box box3">
+                <StatisticsWidget title="Total Revenue" value={totalRevenue} />
+            </div>
             <div className="box box4">Upcoming Appointments</div>
             <div className="box box5">Client Satisfaction</div>
             <div className="box box6">Upcoming Tasks</div>
