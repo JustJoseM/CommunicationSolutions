8957c8d710ffa250ebbda8d99dadfaf40e1bd5f7
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-10-15 16:01:31 -0700

    Adjusted carousel settings on client testimonial page >> Added a dummy client >> Changed bg colour for carousel cards >> Moved the client testimonial data onto a separate js file and mapped it

--- src/Pages/TestimonialTest.js
+++ src/Pages/TestimonialTest.js
@@ -1,23 +1,39 @@
 import React from "react";
 import Slider from "react-slick";
 
-import '../PagesCSS/TestimonialTest.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import USAPRO_logo from '../assets/USAPRO.jpg';
-import ClearPRO_logo from '../assets/ClearPRO.png';
-import APEX_logo from '../assets/APEX.jpg';
+import '../PagesCSS/TestimonialTest.css';
+import { clientTestimonials } from "../Data/clientsTestimonial";
 
 const Testimonial = () => {
     const settings = {
+        accessibility: true,
+        arrows: true,
+        centerMode: true,
+        centerPadding: "40px",
         dots: true,
         infinite: true,
+        lazyLoad: "ondemand",
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 15000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     };
 
     return (
@@ -25,40 +41,22 @@ const Testimonial = () => {
             <div className="testimonial-container">
                 <h1 className="testimonial-header">Who We've Worked With</h1>
                 <div className="header-divider"></div>
+
+                {/* Slider component */}
                 <Slider {...settings}>
-                    <div className="testimonial-slide">
-                        <div className="testimonial-card">
-                            <img src={USAPRO_logo} alt="USA PRO" className="company-logo"/>
+                    {clientTestimonials.map(testimonial => (
+                        <div className="testimonial-slide" key={testimonial.id}>
+                        <div className="testimonial-card"> {/* Box to hold the review */}
+                            <img src={testimonial.img} alt={testimonial.name} className="company-logo"/>
                             <div className="company-name">
-                                <strong>USA PRO</strong>
+                                <strong>{testimonial.name}</strong>
                             </div>
                             <p className="testimonial-description">
-                                Review goes here
+                                {testimonial.review}
                             </p>
                         </div>
                     </div>
-                    <div className="testimonial-slide">
-                        <div className="testimonial-card">
-                            <img src={ClearPRO_logo} alt="Clear PRO" className="company-logo"/>
-                            <div className="company-name">
-                                <strong>ClearPro</strong>
-                            </div>
-                            <p className="testimonial-description">
-                                Review goes here
-                            </p>
-                        </div>
-                    </div>
-                    <div className="testimonial-slide">
-                        <div className="testimonial-card">
-                            <img src={APEX_logo} alt="Apex Window Cleaning" className="company-logo"/>
-                            <div className="company-name">
-                                <strong>Apex Window Cleaning</strong>
-                            </div>
-                            <p className="testimonial-description">
-                                Review goes here
-                            </p>
-                        </div>
-                    </div>
+                    ))}
                 </Slider>
             </div>
         </div>
