f5d4d6106af049bd40984b4fa183f9f58af51e5c
Arshmit Singh Bains <arshmitsinghbains@Arshmits-MacBook-Pro.local>
2024-10-15 23:21:32 -0700

    added real clients in admin portal homepage in recent clients section added a picture for client 1

--- src/AdminPortal/AdminComponents/TopBox/TopBox.js
+++ src/AdminPortal/AdminComponents/TopBox/TopBox.js
@@ -1,26 +1,49 @@
+import React, { useEffect, useState } from "react";
 import "./TopBox.css"
-import { recentClients } from "../../data2";
+import { db } from '../../../firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 const TopBox = () => {
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        const fetchClients = async () => {
+            try {
+                const clientCollection = collection(db, 'Clients'); 
+                const clientSnapshot = await getDocs(clientCollection);
+                const clientData = clientSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setClients(clientData);
+            } catch (error) {
+                console.error('Error fetching clients:', error);
+            }
+        };
+
+        fetchClients();
+    }, []);
+
     return (    
         <div className="topBox">
             <h1>Recent Clients</h1>
             <div className="list">
-                {recentClients.map(recentClients=>(
+                {clients.map(recentClients=>(
                     <div className="topBoxlistItem" key={recentClients.id}>
                         <div className="client">
-                            <img src={recentClients.img} alt=""/>
+                            <img src={recentClients.Photo} alt=""/>
                             <div className="clientTexts">
-                                <span className="name">{recentClients.name}</span>
-                                <span className="email">{recentClients.email}</span>
+                                <span className="name">{recentClients.CompanyName}</span>
+                                <span className="email">{recentClients.Email}</span>
                             </div>
                         </div>
-                        <span className="name">{recentClients.number}</span>
+                        
                     </div>
                 ))}
             </div>
         </div> 
     )
-}
+};
+
+
+
+
 
 export default TopBox;
