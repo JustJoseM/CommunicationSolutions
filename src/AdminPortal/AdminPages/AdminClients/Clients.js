92faeba58377b990b17c6b8f9407ec7cf6ad37a7
Arshmit Singh Bains <arshmitsinghbains@Arshmits-MacBook-Pro.local>
2024-10-15 22:30:51 -0700

    added real clients in admin portal in clients section

--- src/AdminPortal/AdminPages/AdminClients/Clients.js
+++ src/AdminPortal/AdminPages/AdminClients/Clients.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Clients.css';
-import { recentClients } from "../../data2";
+import { db } from '../../../firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Clients = () => {
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        const fetchClients = async () => {
+            try {
+                const clientCollection = collection(db, 'Clients'); 
+                const clientSnapshot = await getDocs(clientCollection);
+                const clientData = clientSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setClients(clientData);
+            } catch (error) {
+                console.error('Error fetching clients:', error);
+            }
+        };
+
+        fetchClients();
+    }, []);
+
     return (
         <div className="ClientPage">
-            <h1 className="clientInfo">Client information</h1>
+            <h1 className="clientInfo">Client Information</h1>
             <div className="boxHolder">
-                {recentClients.map(client => (
+                {clients.map(client => (
                     <div key={client.id} className={`clientBox box${client.id}c`}>
-                        <img src={client.img} alt={client.name} className="clientImg"/>
-                        <h2 className="clientName">{client.name}</h2>
-                        <p className="clientEmail">Email: {client.email}</p>
-                        <p className="clientPhone">Phone Number: {client.number}</p>
+                        <img src={client.Photo} alt={client.name} className="clientImg"/>
+                        <h2 className="clientName">{client.CompanyName}</h2>
+                        <p className="clientEmail">Email: {client.Email}</p>
+                        <p className="clientPhone">Phone Number: {client.Contact}</p>
                     </div>
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Clients;
