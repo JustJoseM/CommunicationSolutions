f3c63656cf5eebb3e45af519b75f49afd710c6d9
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-10-28 21:29:12 -0700

    General Settings page functionality, connected to backend/firebase

--- src/AdminPortal/AdminPages/AdminSettings/SettingsPages/SchedulingSettings.js
+++ src/AdminPortal/AdminPages/AdminSettings/SettingsPages/SchedulingSettings.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { db } from "../../../../firebaseConfig";
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import moment from 'moment-timezone';
 import "../SettingsPagesCSS/SchedulingSettings.css";
 
 const SchedulingOptions = () => {
-  const [timezones, setTimezones] = useState([]);
   const [settings, setSettings] = useState({
     DefaultApptType: '',
     DefaultDuration: '',
@@ -18,12 +16,6 @@ const SchedulingOptions = () => {
     NotifyChanges: false,
   });
 
-  useEffect(() => {
-    // Get a list of available timezones
-    const availableTimezones = moment.tz.names();
-    setTimezones(availableTimezones);
-    fetchSettings();
-  }, []);
 
   // Fetch the preferences off Firebase
   const fetchSettings = async () => {
@@ -61,15 +53,20 @@ const SchedulingOptions = () => {
     }));
   };
 
+  useEffect(() =>{
+    fetchSettings();
+  }, [])
+
   return(
     <div className="scheduling-settings">
-      <h2>Scheduling Settings</h2>
+      <h2 className="scheduling-settings-header">Scheduling Settings</h2>
 
       {/* Section for General Scheduling settings */}
       <section className="general-scheduling-settings">
+        <h2 className="general-settings">General Scheduling Settings</h2>
         <label>
           Default Appointment Type:
-          <select name="DefaultApptType" value={settings.DefaultApptType} onChange={handleChange}>
+          <select className="default-apt-select" name="DefaultApptType" value={settings.DefaultApptType} onChange={handleChange}>
             <option value="meeting">Meeting</option>
             <option value="consultation">Consultation</option>
             <option value="follow-up">Follow Up</option>
@@ -89,7 +86,7 @@ const SchedulingOptions = () => {
         {/* Cancellation Policy */}
         <label>
           Cancellation Policy (Notice Required):
-          <select name="CancellationTime" value={settings.CancellationTime} onChange={handleChange}>
+          <select className= "cancellation-policy" name="CancellationTime" value={settings.CancellationTime} onChange={handleChange}>
             <option value="0">None</option>
             <option value="24">24 hours</option>
             <option value="48">48 hours</option>
@@ -99,7 +96,7 @@ const SchedulingOptions = () => {
 
         <label>
           Default Appointment Duration:
-          <select name="DefaultDuration" value={settings.DefaultDuration} onChange={handleChange}>
+          <select className="default-duration" name="DefaultDuration" value={settings.DefaultDuration} onChange={handleChange}>
             <option value="15">15 minutes</option>
             <option value="30">30 minutes</option>
             <option value="60">1 hour</option>
@@ -108,7 +105,7 @@ const SchedulingOptions = () => {
 
         <label>
           Default Meeting Type:
-          <select name="DefaultType" value={settings.DefaultType} onChange={handleChange}>
+          <select className="default-meeting-type" name="DefaultType" value={settings.DefaultType} onChange={handleChange}>
             <option value="zoom">Zoom</option>
             <option value="teams">Microsoft Teams</option>
             <option value="in-person">In-Person</option>
@@ -118,7 +115,10 @@ const SchedulingOptions = () => {
 
       {/* Section for Notification Preferences regarding Appointments */}
       <section className="scheduling-notification-settings">
-        <h3>Appointment Notification Preferences</h3>
+        <h3 className="apt-notif-prefs">Appointment Notification Preferences</h3>
+        <label>
+          <input type="checkbox" name="NotifyChanges" checked={settings.NotifyChanges} onChange={handleChange} />Notify Participants of Changes
+        </label>
         <label>
           <input type="checkbox" name="AppointmentReminder" checked={settings.AppointmentReminder} onChange={handleChange}/>
           Enable Reminder Notifications
@@ -126,16 +126,13 @@ const SchedulingOptions = () => {
 
         <label>
           Reminder Time:
-          <select name="ReminderTime" value={settings.ReminderTime} onChange={handleChange}>
+          <select className="reminder-time" name="ReminderTime" value={settings.ReminderTime} onChange={handleChange}>
+            <option value="0">None</option>
             <option value="5">5 minutes before</option>
             <option value="15">15 minutes before</option>
             <option value="30">30 minutes before</option>
             <option value="60">1 hour before</option>
           </select>
-        </label>
-
-        <label>
-          <input type="checkbox" name="NotifyChanges" checked={settings.NotifyChanges} onChange={handleChange} />Notify Participants of Changes
         </label>
       </section>
 
